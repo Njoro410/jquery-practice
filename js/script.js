f9348c77325d13154cd32bd5f867f2be668fd5f2
Njoro410 <brian.njoroge@student.moringaschool.com>
2021-12-15 16:16:39 +0300

    added all required code

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+$(document).ready(function () {
+    $(".clickable").click(function () {
+        $("img").show();
+        $("#walrus-showing").slideToggle();
+        $("#walrus-hidden").slideToggle();
+    });
+});
+
